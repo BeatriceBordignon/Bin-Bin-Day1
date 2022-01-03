730197627d7499190b6c5b87f4ae5076c2346061
OssianN <ossian.northen@gmail.com>
2022-01-03 15:08:00 +0100

    initial commit

---
+++ Solutions.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace miniKatas;
+
+public class MathFunctions
+{
+    public int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+    public int Divide(int a, int b)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class ListFunctions
+{
+    public int Smallest(int[] inputArr)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int[] Sort(int[] inputArr)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class StringFunctions
+{
+    public string NoSpaces(string input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string EmailMaker(string input)
+    {
+        throw new NotImplementedException();
+    }
+}
